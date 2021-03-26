505d8435577bd29f55239ce729a16b168c3f4af6
Guttume <guttume@outlook.com>
2021-03-26 18:25:35 +0530

    added commenting system with utterances

--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -25,6 +25,16 @@
 
     <div class="border-b border-blue-200 mb-10 pb-4" v-pre>
         @yield('content')
+
+        <div>
+            <script src="https://utteranc.es/client.js"
+                    repo="guttume/blog"
+                    issue-term="url"
+                    theme="github-light"
+                    crossorigin="anonymous"
+                    async>
+            </script>
+        </div>
     </div>
 
     <nav class="flex justify-between text-sm md:text-base">
